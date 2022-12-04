7541f0e4f253dc6f538a20ebf3b71bb06400a863
Jonathan Marpaung <60574415+jonathanmarp@users.noreply.github.com>
2022-12-04 15:37:14 +0700

    Add files via upload

--- Main/src/root.cpp
+++ Main/src/root.cpp
@@ -8,6 +8,8 @@
 // @system include - BEGIN
 #include <system.h>
 #include <text.h>
+#include <physics.h>
+#include <typedef.h>
 // @system include - END
 
 #include "../include/root.h"
@@ -85,6 +87,140 @@ namespace GameFrameWork
 		}
 	};
 
+	struct PhysicsBoxGround : public Controller
+	{
+		SDL_Rect Rect = {};
+
+		PhysicsBoxGround(Physics::Core& world) : Controller()
+		{
+			// Add Physics World
+			PhysicsWorld& world_ = this->AddComponent<PhysicsWorld>(world());
+
+			// Position And Size
+			auto& position = this->AddComponent<Component::Physics::Position>();
+			auto& size = this->AddComponent<Component::Physics::Size>();
+
+			position.X = 400;
+			position.Y = 500;
+			size.Width = 200;
+			size.Height = 40;
+
+			// Make Ground
+			b2BodyDef groundBodyDef;
+			groundBodyDef.position.Set(position.X, position.Y);
+
+			b2Body* groundBody = world_->CreateBody(&groundBodyDef);
+
+			b2PolygonShape groundBox;
+			groundBox.SetAsBox(size.Width * 0.5f, size.Height * 0.5f);
+
+			groundBody->CreateFixture(&groundBox, 0.0f);
+
+			// Add Body
+			this->AddComponent<b2Body*>(groundBody);
+		}
+
+		void Update()
+		{
+			// Get Component
+			auto& position = this->GetComponent<Component::Physics::Position>();
+			b2Body* body = this->GetComponent<b2Body*>();
+
+			// Update
+			const b2Vec2& positionBody = body->GetPosition();
+			position.X = positionBody.x;
+			position.Y = positionBody.y;
+		}
+
+		void Render()
+		{
+			// Position And Size
+			auto& position = this->GetComponent<Component::Physics::Position>();
+			auto& size = this->GetComponent<Component::Physics::Size>();
+
+			Rect = std::move(Physics::Core::GetWorldCenter(
+				position, size));
+
+			SDL_SetRenderDrawColor(
+				System::Renderer, 0x91, 0xC7, 0x88, 0x00);
+			SDL_RenderFillRect(System::Renderer, &Rect);
+		}
+	};
+
+	struct PhysicsBox : public Controller
+	{
+		SDL_Rect Rect = {};
+
+		PhysicsBox(Physics::Core& world) : Controller()
+		{
+			// Add Physics World
+			PhysicsWorld& world_ = this->AddComponent<PhysicsWorld>(world());
+		
+			// Add Ground
+			this->AddComponent<PhysicsBoxGround>(PhysicsBoxGround(world));
+
+			// Position And Size
+			auto& position = this->AddComponent<Component::Physics::Position>();
+			auto& size = this->AddComponent<Component::Physics::Size>();
+
+			position.X = 400;
+			position.Y = 100;
+			size.Width = 40;
+			size.Height = 40;
+
+			// Make Body
+			b2BodyDef bodyDef;
+			bodyDef.type = b2_dynamicBody;
+			bodyDef.position.Set(position.X, position.Y);
+			b2Body* body = world_->CreateBody(&bodyDef);
+
+			b2PolygonShape dynamicBox;
+			dynamicBox.SetAsBox(
+				size.Width * 0.5f, size.Height * 0.5f);
+
+			b2FixtureDef fixtureDef;
+			fixtureDef.shape = &dynamicBox;
+			fixtureDef.density = 1.0f;
+			fixtureDef.friction = 0.3f;
+
+			body->CreateFixture(&fixtureDef);
+
+			// Add Body
+			this->AddComponent<b2Body*>(body);
+		}
+
+		void Update()
+		{
+			// Get Component
+			auto& position = this->GetComponent<Component::Physics::Position>();
+			b2Body* body = this->GetComponent<b2Body*>();
+
+			// Update
+			const b2Vec2& positionBody = body->GetPosition();
+			position.X = positionBody.x;
+			position.Y = positionBody.y;
+
+			this->GetComponent<PhysicsBoxGround>().Update();
+		}
+
+		void Render()
+		{
+			// Position And Size
+			auto& position = this->GetComponent<Component::Physics::Position>();
+			auto& size = this->GetComponent<Component::Physics::Size>();
+
+			Rect = std::move(Physics::Core::GetWorldCenter(
+				position, size));
+
+			SDL_SetRenderDrawColor(
+				System::Renderer, 0xFE, 0xFF, 0xDE, 0x00);
+			SDL_RenderFillRect(System::Renderer, &Rect);
+
+			// Render Ground
+			this->GetComponent<PhysicsBoxGround>().Render();
+		}
+	};
+
 	void Root::Start()
 	{
 		// Test
@@ -93,8 +229,13 @@ namespace GameFrameWork
 		// Setting Window
 		System::Config.Background = SDL_Color({ 0xF2, 0xF5, 0xC8 });
 
-		// Add Bouncing Box
+		// Add Physics Core
+		Physics::Core& PhysicsCore = 
+			this->AddComponent<Physics::Core>(Physics::Core());
+
+		// Add Bouncing Box and Physics Box
 		this->AddComponent<BouncingBox>(BouncingBox());
+		this->AddComponent<PhysicsBox>(PhysicsBox(PhysicsCore));
 
 		// Add Font Family
 		Text::AddFontFamily(
@@ -129,6 +270,8 @@ namespace GameFrameWork
 	void Root::Update()
 	{
 		this->GetComponent<BouncingBox>().Update();
+		this->GetComponent<Physics::Core>().Update();
+		this->GetComponent<PhysicsBox>().Update();
 	}
 
 	void Root::Render()
@@ -137,6 +280,7 @@ namespace GameFrameWork
 		auto& text = this->GetComponent<HelperText>();
 
 		this->GetComponent<BouncingBox>().Render();
+		this->GetComponent<PhysicsBox>().Render();
 
 		// Render all texts
 		for (size_t idx = 0; idx < text.Texts.size(); idx++)
